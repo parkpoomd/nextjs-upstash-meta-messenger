367c0101d399dc4eb0ff790c5e06e1a81aa6d722
parkpoomd <parkpoom.d@gmail.com>
2023-01-16 08:45:52 +0700

    env

--- pusher.ts
+++ pusher.ts
@@ -2,14 +2,14 @@ import Pusher from "pusher";
 import ClientPusher from "pusher-js";
 
 export const serverPusher = new Pusher({
-  appId: "1513465",
-  key: "2b278b2911a8c544b2f9",
-  secret: "6adc4cdd912056267d61",
-  cluster: "ap1",
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
   useTLS: true,
 });
 
-export const clientPusher = new ClientPusher("2b278b2911a8c544b2f9", {
+export const clientPusher = new ClientPusher(process.env.PUSHER_CLIENT, {
   cluster: "ap2",
   forceTLS: true,
 });
